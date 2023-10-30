05c8d618461eb6a52f78a1d91c2321aca61186c1
indiamaris <indiamaris@gmail.com>
2023-10-30 16:22:47 +0100

    reactQuery

---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,50 @@
+/** @format */
+
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+interface Todo {
+	id: number;
+	title: string;
+	userId: number;
+	completed: boolean;
+}
+
+const TodoList = () => {
+	const fetchTodos = () =>
+		axios
+			.get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+			.then((res) => res.data);
+	const {data: todos} = useQuery({
+		queryKey: ['todos'],
+		queryFn: fetchTodos,
+	});
+
+	// const [todos, setTodos] = useState<Todo[]>([]);
+	// const [error, setError] = useState('');
+
+	// useEffect(() => {
+	// 	axios
+	// 		.get('https://jsonplaceholder.typicode.com/todos')
+	// 		.then((res) => setTodos(res.data))
+	// 		.catch((error) => setError(error));
+	// }, []);
+
+	// if (error) return <p>{error}</p>;
+
+	return (
+		<ul className='list-group'>
+			{todos?.map((todo) => (
+				<li
+					key={todo.id}
+					className='list-group-item'>
+					{todo.title}
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default TodoList;
+
